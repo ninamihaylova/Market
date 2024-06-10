0d861e9af5d520cd937a960dc29d7f2bc1a99fbe
ninamihaylova <163305180+ninamihaylova@users.noreply.github.com>
2024-06-10 22:30:50 +0300

    Initial commit

---
+++ Market/Program.cs
@@ -0,0 +1,14 @@
+﻿class Market
+{
+    static void Main(string[] args)
+    {
+        
+        decimal tomatoPrice = decimal.Parse(Console.ReadLine());
+        double tomatoQuantity = double.Parse(Console.ReadLine());
+        decimal cucumberPrice = decimal.Parse(Console.ReadLine());
+        double cucumberQuantity = double.Parse(Console.ReadLine());
+
+        decimal totalCost = tomatoPrice * (decimal)tomatoQuantity + cucumberPrice * (decimal)cucumberQuantity;
+        Console.WriteLine($"{totalCost:F2}");
+    }
+    }
